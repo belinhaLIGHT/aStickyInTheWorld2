995dc814151c28e226ad9af9d70c9bc07d56147f
belinhaLIGHT <154287268+belinhaLIGHT@users.noreply.github.com>
2024-11-26 20:56:55 -0300

    Add files via upload

---
+++ scrioot.js
@@ -0,0 +1,93 @@
+function pag1() {
+    window.location = "1-1.html";
+}
+
+function pag2() {
+    window.location = "1-2.html";
+}
+
+function pagmorrer(){
+    window.location = "morrerlvl1.html";
+}
+
+function pag3() {
+    window.location = "1-3.html";
+}
+
+function pag4() {
+    window.location = "1-4.html";
+}
+
+function pag5() {
+    window.location = "1-5.html";
+}
+
+function pag6() {
+    window.location = "1-6.html";
+}
+
+function pag7() {
+    window.location = "2-1.html"
+}
+
+function pag8() {
+    window.location = "2-2.html"
+}
+
+function pagmorrer2() {
+        window.location = "pagmorrer2.html"
+}
+
+function pag9() {
+    window.location = "2-3.html"
+}
+
+function pag10() {
+    window.location = "2-4.html"
+}
+
+function pag11() {
+    window.location = "2-5.html"
+}
+
+function pag12() {
+    window.location = "2-6.html"
+}
+
+function pag13() {
+    window.location = "2-7.html"
+}
+
+function pag14() {
+    window.location = "3-1.html"
+}
+function pag15() {
+    window.location = "3-2.html"
+}
+function pag16() {
+    window.location = "3-3.html"
+}
+function pag17() {
+    window.location = "3-4.html"
+}
+function pag18() {
+    window.location = "3-5.html"
+}
+function pag19() {
+    window.location = "3-6.html"
+}
+function pag20() {
+    window.location = "3-7.html"
+}
+function pag21() {
+    window.location = "4-1.html"
+}
+function pagmorrer3() {
+    window.location = "morrerlvl3.html"
+}
+function pagmorrer4() {
+    window.location = "morrerlvl4.html"
+}
+
+
+
